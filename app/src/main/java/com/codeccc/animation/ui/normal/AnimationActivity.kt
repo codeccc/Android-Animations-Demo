40193e42acb3a903f953019f5b8bf44f0cf1fdd5
wangbo <wb1276831936@163.com>
2021-08-09 12:51:49 +0800

    调整属性动画实现

--- app/src/main/java/com/codeccc/animation/ui/normal/AnimationActivity.kt
+++ app/src/main/java/com/codeccc/animation/ui/normal/AnimationActivity.kt
@@ -1,6 +1,7 @@
 package com.codeccc.animation.ui.normal
 
 import android.animation.Animator
+import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.animation.*
@@ -29,14 +30,16 @@ class AnimationActivity : AppCompatActivity() {
         binding.apply {
             btnAnim1.setOnClickListener {
                 //缩放动画
+                //使用ValueAnimator实现缩放动画
                 val valueAnim = ValueAnimator.ofFloat(1f, 1.5f, 0.5f, 1f)
                 valueAnim.setInterpolator { input ->
 
                     return@setInterpolator (Math.cos((input + 1) * Math.PI) / 2.0f).toFloat() + 0.5f
                 }
-                valueAnim.setEvaluator { fraction, startValue, endValue ->
-//                    btnAnim1.scaleX = startValue
-//                    btnAnim1.scaleY = startValue
+                valueAnim.addUpdateListener {
+                    val scale = it.animatedValue as Float
+                    animationView.scaleX = scale
+                    animationView.scaleY = scale
                 }
                 valueAnim.setDuration(500)
                 valueAnim.start()
@@ -44,10 +47,61 @@ class AnimationActivity : AppCompatActivity() {
 
             btnAnim2.setOnClickListener {
                 //平移动画动画
-                btnAnim2.animate()
-                    .translationY(300f)
+                val transAnim1 = ValueAnimator.ofFloat(0f, 100f, 200f, 100f, 0f)
+                transAnim1.addUpdateListener {
+                    val distance = it.animatedValue as Float
+                    animationView.translationX = distance
+                    animationView.translationY = distance
+                }
+
+                val transAnim2 = ValueAnimator.ofFloat(0f, -100f, -200f, -100f, 0f)
+                transAnim2.addUpdateListener {
+                    val distance = it.animatedValue as Float
+                    animationView.translationX = distance
+                    animationView.translationY = distance
+                }
+                //使用AnimatorSet处理动画播放顺序
+                val animatorSet = AnimatorSet().apply {
+                    play(transAnim1).before(transAnim2)
+                }
+                animatorSet.interpolator = AccelerateDecelerateInterpolator()
+                animatorSet.duration = 400
+                animatorSet.start()
+            }
+
+            btnAnim3.setOnClickListener {
+                //旋转动画
+                animationView.animate()
+                    .rotation(animationView.rotation + 30f)
+                    //越界回弹差值器
+                    .setInterpolator(AnticipateOvershootInterpolator())
                     .setDuration(500)
-                    .setInterpolator(OvershootInterpolator(2f))
+                    .start()
+            }
+
+            btnAnim4.setOnClickListener {
+                //淡入淡出动画
+                val alphaAnimator = ValueAnimator.ofFloat(0f, 1f, 0f, 1f)
+                alphaAnimator.interpolator = AccelerateDecelerateInterpolator()
+                alphaAnimator.duration = 1000
+                alphaAnimator.addUpdateListener { animationView.alpha = it.animatedValue as Float }
+                alphaAnimator.start()
+            }
+
+            btnAnim5.setOnClickListener {
+                //组合动画 缩放+平移+旋转+淡入
+                //设置初始状态
+                animationView.scaleX = 0.5f
+                animationView.scaleY = 0.5f
+                animationView.alpha = 0f
+                animationView.rotation = 0f
+                animationView.animate()
+                    .translationY(100f)
+                    .scaleX(1.5f).scaleY(1.5f)
+                    .alpha(1f)
+                    .rotation(360f)
+                    .setDuration(500)
+                    .setInterpolator(AnticipateOvershootInterpolator())
                     .setListener(object : Animator.AnimatorListener {
                         override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
                         }
@@ -56,16 +110,15 @@ class AnimationActivity : AppCompatActivity() {
                         }
 
                         override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                            super.onAnimationEnd(animation, isReverse)
                         }
 
                         override fun onAnimationEnd(animation: Animator?) {
-                            //回到原有位置
-//                            btnAnim2.animate()
-//                                .translationY(0f)
-//                                .setDuration(500)
-//                                .setInterpolator(BounceInterpolator())
-//                                .setStartDelay(2000)
-//                                .start()
+                            //回到动画初始状态
+                            animationView.scaleX = 0.5f
+                            animationView.scaleY = 0.5f
+                            animationView.alpha = 1f
+                            animationView.rotation = 0f
                         }
 
                         override fun onAnimationCancel(animation: Animator?) {
@@ -74,30 +127,6 @@ class AnimationActivity : AppCompatActivity() {
                         override fun onAnimationRepeat(animation: Animator?) {
                         }
                     })
-                    .start()
-            }
-
-            btnAnim3.setOnClickListener {
-                //旋转动画
-            }
-
-            btnAnim4.setOnClickListener {
-                //淡入淡出动画
-            }
-
-            btnAnim5.setOnClickListener {
-                //组合动画
-                btnAnim5.scaleX = 0.5f
-                btnAnim5.scaleY = 0.5f
-                btnAnim5.alpha = 0f
-                btnAnim5.rotation = 0f
-                btnAnim5.animate()
-                    .translationY(100f)
-                    .scaleX(1.5f).scaleY(1.5f)
-                    .alpha(1f)
-                    .rotation(360f)
-                    .setDuration(500)
-                    .setInterpolator(AccelerateDecelerateInterpolator())
                     .start()
             }
         }
